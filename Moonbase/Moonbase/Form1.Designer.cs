05f3eb6ca7feae411a22fa29a0f5e5a9f4c2c24c
LuiLawless <rdelgado84872@uat.edu>
2024-07-13 22:00:12 -0400

    Add files via upload

---
+++ Moonbase/Moonbase/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Moonbase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBlobby = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBdescription = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TBdescription);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.TBlobby);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 257);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Location Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Room Name";
+            // 
+            // TBlobby
+            // 
+            this.TBlobby.Location = new System.Drawing.Point(23, 37);
+            this.TBlobby.Name = "TBlobby";
+            this.TBlobby.ReadOnly = true;
+            this.TBlobby.Size = new System.Drawing.Size(267, 20);
+            this.TBlobby.TabIndex = 1;
+            this.TBlobby.Text = "Lobby";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Room Description";
+            // 
+            // TBdescription
+            // 
+            this.TBdescription.Location = new System.Drawing.Point(26, 81);
+            this.TBdescription.Multiline = true;
+            this.TBdescription.Name = "TBdescription";
+            this.TBdescription.ReadOnly = true;
+            this.TBdescription.Size = new System.Drawing.Size(264, 139);
+            this.TBdescription.TabIndex = 3;
+            this.TBdescription.Text = resources.GetString("TBdescription.Text");
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(667, 345);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Outlaws Hideaway";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBdescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBlobby;
+    }
+}
+
---
+++ Moonbase/Moonbase/Form1.cs
@@ -0,0 +1,44 @@
+﻿//Assignment 1.3: Moonbase by Robert Delgado
+//With assistance from my teacher Mrs. Coddington and some assistance
+//from OpenAI chatgpt
+
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Moonbase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the height and width of the form
+            this.Height = 1080;
+            this.Width = 1920;
+
+            // Define the path to the moonbase background image
+            string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "debug", "moonbase.jpeg");
+
+            // Check if the image file exists. OpenAI insisted I needed to use a
+            //if statement to make sure the file path existed. We didnt cover this in our
+            //lecture sessions, so I had to research. 
+            if (File.Exists(imagePath))
+            {
+                // Set the background image
+                this.BackgroundImage = Image.FromFile(imagePath);
+                // Set the background image layout to stretch
+                this.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else
+            {
+                // Display an error message if the image file is not found because we
+                //to display an error.
+                MessageBox.Show("Image not found: " + imagePath);
+            }
+        }
+    }
+}
